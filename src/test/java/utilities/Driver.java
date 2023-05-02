8b18bfbaa4432aa890960658048fb51db0044d3c
samet ozcelik <smtab142790@gmail.com>
2023-05-02 16:13:58 +0300

    02/05/2023

---
+++ src/test/java/utilities/Driver.java
@@ -0,0 +1,28 @@
+package utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class Driver {
+    /*
+    POM de Driver icin TestBase classina extends etmek yerine Driver class indan
+    static methodlar kullanarak driver olusturup, ilgili ayarlarin yapilmasi
+    ve en sonda driver in kapatilmasi tercih edilmiştir
+     */
+    static WebDriver driver;
+    public static WebDriver getDriver(){
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.manage().window().maximize();
+
+        return driver;
+    }
+    public static void closeDriver(){
+
+        driver.close();
+    }
+}
