f899fbf6af86c24b00b418b05bbc16f039266a6e
samet ozcelik <smtab142790@gmail.com>
2023-05-06 13:20:07 +0300

    05/05/2023

--- src/test/java/pages/AmazonPage.java
+++ src/test/java/pages/AmazonPage.java
@@ -13,7 +13,7 @@ public class AmazonPage {
 
 
     @FindBy(id="twotabsearchtextbox")
-    public WebElement aramaKutusu;
+    public static WebElement aramaKutusu;
 
     @FindBy(xpath = "//div[@class='a-section a-spacing-small a-spacing-top-small']")
     public WebElement aramaSonucElementi;
