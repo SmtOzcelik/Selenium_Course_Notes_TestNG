1c5d8a0fe43a8b949dd438d00ac8992cd4936643
samet ozcelik <smtab142790@gmail.com>
2023-05-06 17:47:15 +0300

    05/05/2023

---
+++ src/test/java/tests/day22_crossBrowser/C01_AssertionsCross.java
@@ -0,0 +1,37 @@
+package tests.day22_crossBrowser;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utilities.TestBase;
+import utilities.TestBaseCross;
+
+public class C01_AssertionsCross extends TestBaseCross {
+    // 1-amazon git
+    // 2-title in amazon icerdigini test edelim
+    // 3-arama kutusunun erisebilirligini old. test edin
+    // 4-arama kutusuna nutella yazip arattirin
+    // 5-arama yapildigini test edin
+    // 6-arama sonucunu Nutella icerdigini test edin
+
+    @Test
+    public void test01() {
+        // 1-amazon git
+        driver.get("https://www.amazon.com");
+        // 2-title in Amazon icerdigini test edelim
+        String expectedTitle="amazon";
+        Assert.assertTrue(driver.getTitle().contains(expectedTitle));
+        // 3-arama kutusunun erisebilirligini old. test edin
+        WebElement aramaKutusu=driver.findElement(By.id("twotabsearchtextbox"));
+        Assert.assertTrue(aramaKutusu.isEnabled());
+        // 4-arama kutusuna nutella yazip arattirin
+        aramaKutusu.sendKeys("Nutella", Keys.ENTER);
+        // 5-arama yapildigini test edin
+        WebElement sonucYaziElementi= driver.findElement(By.xpath("//div[@class='a-section a-spacing-small a-spacing-top-small']"));
+        Assert.assertTrue(sonucYaziElementi.isDisplayed());
+        // 6-arama sonucunu Nutella icerdigini test edin
+        Assert.assertTrue(sonucYaziElementi.getText().contains("Kutella"));
+    }
+}
