f680f23a3fcd0e857670a3b979fe239619019148
samet ozcelik <smtab142790@gmail.com>
2023-05-08 09:08:51 +0300

    06/05/2023

--- src/test/java/tests/day22_crossBrowser/C04_NegativeLoginDataProvider.java
+++ src/test/java/tests/day22_crossBrowser/C04_NegativeLoginDataProvider.java
@@ -1,6 +1,7 @@
 package tests.day22_crossBrowser;
 
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pages.BrcPage;
 import utilities.ConfigReader;
@@ -9,17 +10,24 @@ import utilities.Driver;
 public class C04_NegativeLoginDataProvider {
     BrcPage brcPage=new BrcPage();
 
-    @Test
-    public void yanlisSifre() {
+    @DataProvider
+    public static Object[][] kullaniciListesi() {
+        Object[][] kullaniciBilgileri={{"firuze@nehaber.com","5879"},{"murat@mail","12345"},{"samet@gelidi","1245678"}};
+
+        return kullaniciBilgileri;
+    }
+
+    @Test(dataProvider = "kullaniciListesi")
+    public void yanlisSifre(String userEmail,String password) {
         // Bir test method olustur positiveLoginTest()
         // https://bluerentalcars.com git
         Driver.getDriver().get(ConfigReader.getProperty("brcUrl"));
         // login butonuna bas
         brcPage.ilkLoginButonu.click();
-        // test data username:smt@gmail.com
-        brcPage.emailTextBox.sendKeys(ConfigReader.getProperty("brcValidEmail"));
-        // test data password:54321
-        brcPage.passwordTextBox.sendKeys(ConfigReader.getProperty("brcWrongPassword"));
+        // test data username:dataprovider den alallim
+        brcPage.emailTextBox.sendKeys(userEmail);
+        // test data password:dataprovider den alallim
+        brcPage.passwordTextBox.sendKeys(password);
         // login butonuna tikla
         brcPage.ikinciLoginButtonu.click();
         // Degerler girildiginde sayfaya basarili sekilde girilemedigini test et
